d2881139d1b0a5fc3b5b636ca9b7ef322e2477c0
Kelly <kgilliamcode@gmail.com>
2018-01-26 22:19:54 -0800

    [clean] Clean up code

--- src/App.js
+++ src/App.js
@@ -6,39 +6,16 @@ class App extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      // users: [],
-      // dbUsers: [],
+
     }
   }
-
-  // componentDidMount() {
-  //   fetch('/users')
-  //     .then(res => res.json())
-  //     .then(users => this.setState({ users }));
-
-  //   fetch('/retrieveUsers')
-  //     .then(res => res.json())
-  //     .then(dbUsers => this.setState({ dbUsers }));
-  // }
   
   render() {
-
-    // let userNames = [];
-    // for (let i = 0; i < this.state.dbUsers.length; i++) {
-    //   userNames.push(<div>{this.state.dbUsers[i].firstname + ' ' + this.state.dbUsers[i].lastname}</div>);
-    // }
 
     return (
       <div className="App">
         <Timer/>
-        {/* <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
-        <h1>DB Users</h1>
-          {userNames} */}
       </div>
-
     );
   }
 }
