c299aad0ac2213eb1e45e80e6fb122b87ab3fc4c
Kelly <kgilliamcode@gmail.com>
2018-02-16 23:52:28 -0800

    [feature] Add yobasic YouTube video playing on the frontend

--- src/App.js
+++ src/App.js
@@ -61,13 +61,18 @@ class App extends Component {
       console.log(error);
     });
   }
-  
+  // src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com" 
+
   render() {
 
     return (
       <div className="App">
         <Timer/>
         <CookieClicker/>
+        <iframe id="ytplayer" type="text/html" width="640" height="360"
+          src="https://www.youtube.com/embed/xqJBibhR07w"  
+          frameBorder="0">
+        </iframe>
         <button style={{'position': 'absolute', 'top': '100px', 'zIndex': 4}} onClick={() => {this.addUserToUsersTable()}}>{'Add user'}</button>
         <button style={{'position': 'absolute', 'top': '200px', 'zIndex': 4}} onClick={() => {this.verifyUser()}}>{'Verify user'}</button>
         <button style={{'position': 'absolute', 'top': '300px', 'zIndex': 4}} onClick={() => {this.getUsers()}}>{'Get Users'}</button>        
